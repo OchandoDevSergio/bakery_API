22a072066445cb5b9a24bbfaa59e3010bcc8d11b
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-12-07 00:42:03 +0100

    a route changed from get to post

--- views/productionsRouter.js
+++ views/productionsRouter.js
@@ -6,7 +6,11 @@ const productionsController = require('../controllers/productionsController');
 
 router.get('/', productionsController.getAllProductions);
 router.post('/', productionsController.reportProduction);
-router.get('/dates', productionsController.getProductionsInTime);
+router.post('/dates', productionsController.getProductionsInTime);
+
+//Se ha empleado una solicitud POST para traer las producciones filtradas por 
+//fecha en lugar de una solicitud GET, puesto que axios.get no permite mandar un 
+//bodycomo sgundo ardgumento, sólo permite mandar parámetros en la propia URL
 
 
 module.exports = router;
